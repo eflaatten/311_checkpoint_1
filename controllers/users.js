cc5dd036371c92a9bde97a4528daa07e3e91bb58
eflaatten <elliotflaatten@live.com>
2024-06-19 19:12:23 -0500

    controllers, routes and index created

---
+++ controllers/users.js
@@ -0,0 +1,55 @@
+const data = require('../data/index')
+const newUser = require('../data/sampleUser')
+
+const listUsers = (req, res) => {
+  res.json(data)
+}
+
+const showUser = (req, res) => {
+  try {
+    const id = parseInt(req.params.id)
+    const user = data.find(user => user.id === id)
+    if(!user) throw new Error('User not found')
+    res.json(user)
+  } catch (error) {
+    res.status(400).json({ error: 'User not found' })
+  }
+}
+
+const createUser = (req, res) => {
+  try {
+    newUser.id = data.length + 1
+    data.push(newUser)
+    res.json(newUser)
+    if(!newUser) throw new Error('User not created')
+  } catch (error) {
+    res.status(404).json({ error: error.message})
+  }
+}
+
+const updateUser = (req, res) => {
+  try {
+    const id = parseInt(req.params.id)
+    const user = data.find(user => user.id === id)
+    user.name = newUser.name
+    res.json(user)
+    if(!user) throw new Error('User not found')
+  } catch (error) {
+    res.status(400).json({ error: 'User not found' })
+  }
+}
+
+const deleteUser = (req, res) => {
+  try {
+    const id = parseInt(req.params.id)
+    const user = data.find(user => user.id === id)
+    if(!user) throw new Error('User not found')
+    const index = data.indexOf(user)
+    data.splice(index, 1)
+    res.json(user)
+  } catch (error) {
+    res.status(404).json({ error: error.message })
+  }
+}
+
+module.exports = { listUsers, showUser, createUser, updateUser, deleteUser }
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
+const usersRouter = require('./routes/users')
 
 app.get('/', (req, res) => res.send('default route'))
+
+app.use(bodyParser.json())
+app.use(usersRouter)
 
 app.listen(port, () => {
   console.log('app is listening on:', port)
